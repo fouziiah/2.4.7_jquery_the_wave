2f9b8c9f56a45f62a1f1c555b6fa87ff65762ae0
fouziah ahmed <fouziahahmed@fouziahs-MacBook-Air.local>
2023-10-11 10:54:55 -0500

    created nested animation using setinterval

---
+++ script.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+
+    var HEIGHT = $(window).height();
+    var timer = setInterval(wave, 5300);
+
+    function wave() {
+        $('#div1').animate({ top: HEIGHT }, "slow", function () {
+            $('#div2').animate({ top: HEIGHT }, "slow", function () {
+                $('#div3').animate({ top: HEIGHT }, "slow", function () {
+                    $('#div4').animate({ top: HEIGHT }, "slow", function () {
+                        $('#div5').animate({ top: HEIGHT }, "slow", function () {
+                            $('#div6').animate({ top: HEIGHT }, "slow", function () {
+                                $('#div7').animate({ top: HEIGHT }, "slow", function () {
+                                    $('#div8').animate({ top: HEIGHT }, "slow")
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+
+        $('#div1').animate({ top: 0 }, "slow", function () {
+            $('#div2').animate({ top: 0 }, "slow", function () {
+                $('#div3').animate({ top: 0 }, "slow", function () {
+                    $('#div4').animate({ top: 0 }, "slow", function () {
+                        $('#div5').animate({ top: 0 }, "slow", function () {
+                            $('#div6').animate({ top: 0 }, "slow", function () {
+                                $('#div7').animate({ top: 0 }, "slow", function () {
+                                    $('#div8').animate({ top: 0 }, "slow");
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    }
+
+});
+
+
